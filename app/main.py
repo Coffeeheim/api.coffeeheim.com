3946b4cfa2eb3bf40e26569fae18a03b29ca865a
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-09-24 16:39:17 -0300

    add cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+origins = [
+    'https://coffeeheim.com',
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/status')
