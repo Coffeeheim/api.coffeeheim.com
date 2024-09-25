af5923fcc6d8fcbc940b44432ac72be00418c8ad
Sérgio Rafael Siqueira <osergiosiqueira@gmail.com>
2024-09-25 12:54:33 -0300

    status from season

--- app/routers/status.py
+++ app/routers/status.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 from fastapi.responses import JSONResponse
 
+from app.fileutils import json_content
+
+from ..configs import SEASONS, VALHEIM_SERVER
+
 router = APIRouter()
 
 
 @router.get('/status')
 async def status(s: str | None = None) -> JSONResponse:
-    return JSONResponse(content={})
+    return JSONResponse(
+        content=json_content(
+            f'{VALHEIM_SERVER}/{s or SEASONS[0]}/data/htdocs/status.json',
+        )
+    )
